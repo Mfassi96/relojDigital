747f6a928fdc2ed625e9615578fe115650cff8f7
Mfassi96 <59669906+Mfassi96@users.noreply.github.com>
2022-02-08 11:41:42 -0300

    Add files via upload
    versión 2

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 function reloj() {
   let fecha = new Date();
   let horas = document.getElementById("horas");
@@ -17,18 +14,10 @@ function reloj() {
   seg.innerHTML = fecha.getSeconds();
 
   let dia = fecha.getDay();
-  let numDia=fecha.getDate()
+  let numDia = fecha.getDate();
   let mes = fecha.getMonth();
 
-  const dias = [
-    "Dom",
-    "Lun",
-    "Mar",
-    "Mier",
-    "Jue",
-    "Vie",
-    "Sab",
-  ];
+  const dias = ["Dom", "Lun", "Mar", "Mier", "Jue", "Vie", "Sab"];
   const meses = [
     "Enero",
     "Febrero",
@@ -44,65 +33,101 @@ function reloj() {
     "Diciembre",
   ];
 
-  if (dia == 0) {
-    elementoDia.innerHTML = dias[0] + " "+numDia+" / ";
-  } else if (dia == 1) {
-    elementoDia.innerHTML = dias[1] + " "+numDia+" / ";
-  } else if (dia == 2) {
-    elementoDia.innerHTML = dias[2] + " "+numDia+" / ";
-  } else if (dia == 3) {
-    elementoDia.innerHTML = dias[1] + " "+numDia+" / ";
-  } else if (dia == 4) {
-    elementoDia.innerHTML = dias[4] + " "+numDia+" / ";
-  } else if (dia == 5) {
-    elementoDia.innerHTML = dias[5] + " "+numDia+" / ";
-  } else if (dia == 6) {
-    elementoDia.innerHTML = dias[6] + " "+numDia+" / ";
+  switch (dia) {
+    case 0:
+      elementoDia.innerHTML = dias[0] + " " + numDia + " / ";
+      break;
+    case 1:
+      elementoDia.innerHTML = dias[1] + " " + numDia + " / ";
+      break;
+    case 2:
+      elementoDia.innerHTML = dias[2] + " " + numDia + " / ";
+      break;
+
+    case 3:
+      elementoDia.innerHTML = dias[3] + " " + numDia + " / ";
+      break;
+
+    case 4:
+      elementoDia.innerHTML = dias[4] + " " + numDia + " / ";
+      break;
+
+    case 5:
+      elementoDia.innerHTML = dias[5] + " " + numDia + " / ";
+      break;
+
+    case 6:
+      elementoDia.innerHTML = dias[6] + " " + numDia + " / ";
+      break;
+
+    case 7:
+      elementoDia.innerHTML = dias[7] + " " + numDia + " / ";
+      break;
   }
 
-  if (mes == 0) {
-    elementoMes.innerHTML = meses[0] + " / ";
-  } else if (mes == 1) {
-    elementoMes.innerHTML = meses[1] + " / ";
-  } else if (mes == 2) {
-    elementoMes.innerHTML = meses[2] + " / ";
-  } else if (mes == 3) {
-    elementoMes.innerHTML = meses[1] + " / ";
-  } else if (mes == 4) {
-    elementoMes.innerHTML = meses[4] + " / ";
-  } else if (mes == 5) {
-    elementoMes.innerHTML = meses[5] + " / ";
-  } else if (mes == 6) {
-    elementoMes.innerHTML = meses[6] + " / ";
-  } else if (mes == 7) {
-    elementoMes.innerHTML = meses[7] + " / ";
-  } else if (mes == 8) {
-    elementoMes.innerHTML = meses[8] + " / ";
-  } else if (mes == 9) {
-    elementoMes.innerHTML = meses[9] + " / ";
-  } else if (mes == 10) {
-    elementoMes.innerHTML = meses[10] + " / ";
-  } else if (mes == 11) {
-    elementoMes.innerHTML = meses[11] + " / ";
+  switch (mes) {
+    case 0:
+      elementoMes.innerHTML = meses[0] + " / ";
+      break;
+
+    case 1:
+      elementoMes.innerHTML = meses[1] + " / ";
+      break;
+
+    case 2:
+      elementoMes.innerHTML = meses[2] + " / ";
+      break;
+
+    case 3:
+      elementoMes.innerHTML = meses[3] + " / ";
+      break;
+
+    case 4:
+      elementoMes.innerHTML = meses[4] + " / ";
+      break;
+
+    case 5:
+      elementoMes.innerHTML = meses[5] + " / ";
+      break;
+
+    case 6:
+      elementoMes.innerHTML = meses[6] + " / ";
+      break;
+
+    case 7:
+      elementoMes.innerHTML = meses[7] + " / ";
+      break;
+
+    case 8:
+      elementoMes.innerHTML = meses[8] + " / ";
+      break;
+
+    case 9:
+      elementoMes.innerHTML = meses[9] + " / ";
+      break;
+
+    case 10:
+      elementoMes.innerHTML = meses[10] + " / ";
+      break;
+
+    case 11:
+      elementoMes.innerHTML = meses[11] + " / ";
+      break;
   }
 
-  elementoAnio.innerHTML=fecha.getFullYear()
-
-
+  elementoAnio.innerHTML = fecha.getFullYear();
 }
 
-function establecerBorde(){
-    let reloj=document.getElementById('hora');
-    reloj.classList.add('ponerBorde')
+function establecerBorde() {
+  let reloj = document.getElementById("hora");
+  reloj.classList.add("ponerBorde");
 }
 
-function sacarBorde(){
-    let reloj=document.getElementById('hora');
-    reloj.classList.remove('ponerBorde')
+function sacarBorde() {
+  let reloj = document.getElementById("hora");
+  reloj.classList.remove("ponerBorde");
 }
-
 
 setInterval(reloj, 1000);
 setInterval(establecerBorde, 1000);
 setInterval(sacarBorde, 2000);
-
